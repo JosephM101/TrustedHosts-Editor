14e15c49a642efa9ebf121552d78f627a1e93322
Joseph .M 101 <jmig3066@gmail.com>
2021-12-14 18:08:59 -0500

    Change namespace & assembly name

--- TrustedHosts Editor/Properties/Resources.Designer.cs
+++ TrustedHosts Editor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TrustedHosts_Editor.Properties {
+namespace TrustedHostsEditor.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TrustedHosts_Editor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TrustedHosts_Editor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TrustedHostsEditor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- TrustedHosts Editor/Properties/Settings.Designer.cs
+++ TrustedHosts Editor/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TrustedHosts_Editor.Properties {
+namespace TrustedHostsEditor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
