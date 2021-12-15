741eee3943ded6220880c99635fbd2bddcb6ca76
Joseph Mignone <jmig3066@gmail.com>
2021-12-14 22:03:21 -0500

    Revert "Set marquee animation speed"
    This reverts commit 552a7f3efca327b72691d0b73535aaf838bc85f7.

--- TrustedHostsEditor/MainForm.Designer.cs
+++ TrustedHostsEditor/MainForm.Designer.cs
@@ -47,12 +47,11 @@
             this.Hostnames_ListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Hostnames_ListBox.FormattingEnabled = true;
-            this.Hostnames_ListBox.ItemHeight = 16;
-            this.Hostnames_ListBox.Location = new System.Drawing.Point(3, 2);
-            this.Hostnames_ListBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Hostnames_ListBox.Location = new System.Drawing.Point(2, 2);
+            this.Hostnames_ListBox.Margin = new System.Windows.Forms.Padding(2);
             this.Hostnames_ListBox.Name = "Hostnames_ListBox";
             this.Hostnames_ListBox.ScrollAlwaysVisible = true;
-            this.Hostnames_ListBox.Size = new System.Drawing.Size(657, 260);
+            this.Hostnames_ListBox.Size = new System.Drawing.Size(494, 212);
             this.Hostnames_ListBox.TabIndex = 1;
             this.Hostnames_ListBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
             this.Hostnames_ListBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
@@ -60,10 +59,10 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(563, 270);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(422, 219);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 28);
+            this.button1.Size = new System.Drawing.Size(74, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
@@ -72,10 +71,10 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(459, 270);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(344, 219);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 28);
+            this.button2.Size = new System.Drawing.Size(74, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "Remove";
             this.button2.UseVisualStyleBackColor = true;
@@ -87,10 +86,10 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.toolStripProgressBar1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 318);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 257);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(688, 25);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(516, 22);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 4;
             this.statusStrip1.Text = "statusStrip1";
@@ -98,13 +97,12 @@
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 19);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
             // 
             // toolStripProgressBar1
             // 
-            this.toolStripProgressBar1.MarqueeAnimationSpeed = 40;
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(167, 17);
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(125, 16);
             // 
             // backgroundWorker_readTrustedHosts
             // 
@@ -125,19 +123,19 @@
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.Hostnames_ListBox);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(664, 302);
+            this.panel1.Size = new System.Drawing.Size(498, 245);
             this.panel1.TabIndex = 5;
             // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button3.Location = new System.Drawing.Point(3, 270);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(2, 219);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(79, 28);
+            this.button3.Size = new System.Drawing.Size(59, 23);
             this.button3.TabIndex = 4;
             this.button3.Text = "Clear All";
             this.button3.UseVisualStyleBackColor = true;
@@ -145,13 +143,13 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 343);
+            this.ClientSize = new System.Drawing.Size(516, 279);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
